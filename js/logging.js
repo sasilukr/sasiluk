d5760c2655541a97d9c4ad5cc64b2a57d988c221
sasilukr <p.pandapower@gmail.com>
2016-01-03 22:35:52 -0800

    working logging page

--- js/logging.js
+++ js/logging.js
@@ -1,5 +1,10 @@
 Parse.initialize("cM18uI8SLt88aXrUxr2FpsXt06GA1D3Z7ojx0N3L", "mTJXKKWxRzYJ93d1tbuGpcNrFbvEKeWDCJZkYWr5");
 var LogParseTable = Parse.Object.extend("Log");
+
+var parseLogStartKey = "start";
+var parseLogEndKey = "end";
+var parseLogUserIdKey = "userId";
+
 var lastLog;
 $(document).ready(function(){
 	
@@ -7,17 +12,13 @@ $(document).ready(function(){
 
 	$("#loading").hide();
 
-	// TODO decide to display "Start" or "End"
-	if ( !lastLog['end'] ) {
-		$("#submitButton").val("End");
-	} else {
-		$("#submitButton").val("Start");
-	}
-	var nowString = moment().format("YYYY-MM-DD HH:MM");
-	$("#timestamp").val(nowString);
-
-  	// $("#actionButton").click();
-
+  	$("#submitButton").click(function() {
+  		if ( $("#submitButton").val() == "Start" ) {
+  			saveStartTime();
+  		} else {
+  			saveEndTime();
+  		}
+	});
 
 
 });
@@ -25,7 +26,7 @@ $(document).ready(function(){
 function saveStartTime() {
 
 	var startTimestamp = $("#timestamp").val();
-	var start = moment(startTimestamp);
+	var start = moment(startTimestamp).toDate();
 	var log = new LogParseTable();
 	log.set(parseLogUserIdKey, 15);
 	log.set(parseLogStartKey, start);
@@ -33,6 +34,7 @@ function saveStartTime() {
 	log.save(null,  {
 		success: function(log) {
 			console.log('New object created with objectId: ' + log.id);
+			loadLastTimestamp();
 		},
 		error: function(log, error) {
 			console.log('Failed to create new object, with error code: ' + error.message);
@@ -42,6 +44,22 @@ function saveStartTime() {
 
 function saveEndTime() {
 
+	var endTimestamp = $("#timestamp").val();
+	var end = moment(endTimestamp).toDate();
+	if ( lastLog ) {
+		lastLog.set(parseLogEndKey, end);
+
+		lastLog.save(null,  {
+			success: function(log) {
+				console.log('Object updated with objectId: ' + log.id);
+				loadLastTimestamp();	
+			},
+			error: function(log, error) {
+				console.log('Failed to update endTime, with error code: ' + error.message);
+			}
+		});
+	}
+	
 }
 
 function loadLastTimestamp() {
@@ -53,8 +71,21 @@ function loadLastTimestamp() {
 	  console.log("Start" + result.get('start'));
 	  console.log("End" + result.get('end'));
 	  lastLog = result;
-
+	  refreshView();
 	  return result.fetch();
 	});
 
+}
+
+function refreshView() {
+
+	console.log("refresh");
+	var nowString = moment().format("YYYY-MM-DD HH:mm");
+	$("#timestamp").val(nowString);
+
+	if ( lastLog && !lastLog.get('end') ) {
+		$("#submitButton").val("End");
+	} else {
+		$("#submitButton").val("Start");
+	}
 }
